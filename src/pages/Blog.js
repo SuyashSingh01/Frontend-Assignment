4facb7ebfdd584a8abf14a63ecf1e6d9f009a253
Suyashsingh <0808CS211182.ies@ipsacademy.org>
2024-11-09 23:04:47 +0530

    fixed-blog page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Data } from '../Data';
@@ -6,40 +7,38 @@ import PostItemCard from '../components/PostItemCard';
 import HeroSectionContent from '../components/HeroSectionContent';
 import JointeamSection from '../components/JointeamSection';
 import CategoryCard from '../components/CategoryCard';
-import manimage from '../assets/Client-First - IMAGES/man-in-white-dress-shirt-sitting-on-black-rolling-chair-while-facing-black-computer-set-and-smiling-840996.svg';
-
+import manimage from '../assets/Client-First - IMAGES/man-in-white-dress-shirt-sitting-on-black-rolling-chair-while-facing-black-computer-set-and-smiling-840996.svg'
 const Blog = () => {
   const navigate = useNavigate();
   return (
-    <div className="container mx-auto px-4 py-6">
-      {/* Hero Section */}
-      <div className="relative flex flex-col md:flex-row justify-between bg-[#F4F0F8] h-[400px] md:h-[500px] lg:h-[600px] items-center">
-        <HeroSectionContent page={"blog"} />
-        <img
+    <div className="container mx-2 p-6">
+      
+      <div className="relative flex justify-between bg-[#F4F0F8] h-[600px] md:h-[500px] lg:h-[600px]" >
+      <HeroSectionContent page={"blog"} />
+      {/* <img src={manimage} className='w-50% absolute top-14 right-10' alt="manimg" />
+       */}
+       <img
           src={manimage}
           className="hidden md:block mx-auto md:w-1/2 lg:w-[40%] mt-4 md:mt-0 md:absolute md:top-10 right-10 md:right-0 object-contain"
           alt="manimg"
         />
       </div>
-
-      {/* Blog Cards Section */}
+      {/* Blog Cards */}
       <div className="mt-6 mx-4">
-        <h2 className="text-2xl md:text-4xl font-bold font-headingfont p-2 md:p-5">All posts</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center items-center">
-          {Data.map((item, index) => (
+        <h2 className="text-4xl font-bold font-headingfont p-5">All posts</h2>
+        <div className="flex flex-col justify-center items-center w-full gap-y-2 md:gap-y-5">{
+          Data.map((item, index) => (
             <PostItemCard key={index} Item={item} navigate={navigate} />
-          ))}
+          ))
+        }
         </div>
       </div>
-
-      {/* Category Section */}
-      <section className="p-4 md:p-6 bg-white">
-        <div className="mb-4">
-          <h2 className="text-xl md:text-4xl font-bold font-headingfont text-[#232536]">All Categories</h2>
+      <section className="p-6 bg-white">
+      <div className="mb-4">
+          <h2 className="text-xl md:text-4xl font-bold font-headingfont text-[#232536] p-2 md:p-6">All Category</h2>
         </div>
-        <CategoryCard />
+        <CategoryCard/>
       </section>
-
       <JointeamSection />
     </div>
   );
